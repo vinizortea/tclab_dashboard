e9606f35fd55b3276c6674281f844bc26276e579
Vinicius Zortea <vinizortea@gmail.com>
2023-04-03 23:13:11 -0300

    adicionando feature de atualição em tempo real, falta conseguir chamar lab e data de dentro do callback do button

--- pages/pagina_1.py
+++ pages/pagina_1.py
@@ -7,6 +7,14 @@ import pandas as pd
 
 from utils import graficos
 
+def tclab_conditions(n_intervals, lab, data, potencia):
+    lab.Q1(potencia)
+    data.update(n_intervals),
+
+    data_append = pd.DataFrame.from_records(data.log[n_intervals], columns=data.columns)
+
+    return data_append
+
 dash.register_page(__name__, path='/', title='Pagina_1')
 
 data_pag1 = {
@@ -58,6 +66,7 @@ layout = dbc.Container([
                 ]),
                 dcc.Interval(
                     id="interval-component",
+                    disabled=True,
                     interval=1000,
                     n_intervals=0
                 )
@@ -71,13 +80,45 @@ layout = dbc.Container([
 ],fluid=False),
 
 @callback(
-    Output('grafico-temperatura','figure'),
-    Input("interval-component","n_intervals"),
-    State("Submeter","n_clicks")
+    Output("grafico-temperatura","figure"),
+    Output("interval-component","disabled"),
+    Output("interval-component","n_intervals"),
+    Output("info-button","children"),
+    Input("info-button","n_clicks"),
+    State("info-button","children"),
+    State("interval-component","n_intervals"),
+    State("grafico-temperatura","figure"),
+
+    prevent_initial_call = True
 )
-def update_graph(n_intervals):
+def trigger_counter(n_clicks,children,n_intervals,figure):
+    if(children == "Submeter"):
+        # df = pd.DataFrame.from_records(data.log, columns=data.columns)
+        # fig = px.line(df, x='Time', y='T1',markers=True)
+        return False,0,"Parar"
+    elif(children == "Parar"):
+        # lab.close()
+        return True,n_intervals,"Submeter",figure
 
+@callback(
+    Output('grafico-temperatura','extendData'),
+    Input("interval-component","n_intervals"),
+    State('potencia','value'),
+    State('tempo_medicao','value'),
+    State('tempo_aquecimento','value'),
 
+    prevent_initial_call = True
+)
+def update_graph(n_intervals,potencia,tempo_medicao,tempo_aquecimento):
+    if(n_intervals == 0):
+        # global?????
+        lab = tclab.TCLab()
+        data = tclab.Historian(lab.sources)
+
+    return tclab_conditions(n_intervals, lab, data, potencia),[0], 10
+    
+    
+# return graficos.grafico_temperatura(lab, data, potencia, tempo_medicao, tempo_aquecimento)
 
 # Ver dcc.Interval para atualizar página automaticamente, se precisar.
 # Pode ajudar, ou não.
--- testes.py
+++ testes.py
@@ -1,142 +1,32 @@
 import dash
-from dash import dcc, html
-from dash.dependencies import Output, Input
-import plotly.express as px
-import dash_bootstrap_components as dbc
-import pandas as pd
-import pandas_datareader.data as web
-import datetime
+import dash_html_components as html
+import dash_core_components as dcc
+import numpy as np
 
+from dash.dependencies import Input, Output, State
 
-
-# https://stooq.com/
-start = datetime.datetime(2020, 1, 1)
-end = datetime.datetime(2020, 12, 3)
-df = web.DataReader(['AMZN','GOOGL','FB','PFE','MRNA','BNTX'],
-                    'stooq', start=start, end=end)
-# df=df.melt(ignore_index=False, value_name="price").reset_index()
-df = df.stack().reset_index()
-print(df[:15])
-
-# df.to_csv("mystocks.csv", index=False)
-# df = pd.read_csv("mystocks.csv")
-# print(df[:15])
-
-
-# https://www.bootstrapcdn.com/bootswatch/
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
-
-
-# Layout section: Bootstrap (https://hackerthemes.com/bootstrap-cheatsheet/)
-# ************************************************************************
-app.layout = dbc.Container([
-
-    dbc.Row(
-        dbc.Col(html.H1("Stock Market Dashboard",
-                        className='text-center text-primary mb-4'),
-                width=12)
-    ),
-
-    dbc.Row([
-
-        dbc.Col([
-            dcc.Dropdown(id='my-dpdn', multi=False, value='AMZN',
-                         options=[{'label':x, 'value':x}
-                                  for x in sorted(df['Symbols'].unique())],
-                         ),
-            dcc.Graph(id='line-fig', figure={})
-        ],# width={'size':5, 'offset':1, 'order':1},
-           xs=12, sm=12, md=12, lg=5, xl=5
-        ),
-
-        dbc.Col([
-            dcc.Dropdown(id='my-dpdn2', multi=True, value=['PFE','BNTX'],
-                         options=[{'label':x, 'value':x}
-                                  for x in sorted(df['Symbols'].unique())],
-                         ),
-            dcc.Graph(id='line-fig2', figure={})
-        ], #width={'size':5, 'offset':0, 'order':2},
-           xs=12, sm=12, md=12, lg=5, xl=5
-        ),
-
-    ], justify='start'),  # Horizontal:start,center,end,between,around
-
-    dbc.Row([
-        dbc.Col([
-            html.P("Select Company Stock:",
-                   style={"textDecoration": "underline"}),
-            dcc.Checklist(id='my-checklist', value=['FB', 'GOOGL', 'AMZN'],
-                          options=[{'label':x, 'value':x}
-                                   for x in sorted(df['Symbols'].unique())],
-                          labelClassName="mr-3"),
-            dcc.Graph(id='my-hist', figure={}),
-        ], #width={'size':5, 'offset':1},
-           xs=12, sm=12, md=12, lg=5, xl=5
-        ),
-
-        dbc.Col([
-            dbc.Card(
-                [
-                    dbc.CardBody(
-                        html.P(
-                            "We're better together. Help each other out!",
-                            className="card-text")
-                    ),
-                    dbc.CardImg(
-                        src="https://media.giphy.com/media/Ll0jnPa6IS8eI/giphy.gif",
-                        bottom=True),
-                ],
-                style={"width": "24rem"},
-            )
-        ], #width={'size':5, 'offset':1},
-           xs=12, sm=12, md=12, lg=5, xl=5
-        )
-    ], align="center")  # Vertical: start, center, end
-
-], fluid=True)
-
-
-# Callback section: connecting the components
-# ************************************************************************
-# Line chart - Single
-@app.callback(
-    Output('line-fig', 'figure'),
-    Input('my-dpdn', 'value')
+# Example data (a circle).
+resolution = 1000
+t = np.linspace(0, np.pi * 2, resolution)
+x, y = np.cos(t), np.sin(t)
+# Example app.
+figure = dict(data=[{'x': [], 'y': []}], layout=dict(xaxis=dict(range=[-1, 1]), yaxis=dict(range=[-1, 1])))
+app = dash.Dash(__name__, update_title=None)  # remove "Updating..." from title
+app.layout = html.Div([
+    dcc.Graph(id='graph', figure=dict(figure)), dcc.Interval(id="interval", interval=25),
+    dcc.Store(id='offset', data=0), dcc.Store(id='store', data=dict(x=x, y=y, resolution=resolution)),
+])
+app.clientside_callback(
+    """
+    function (n_intervals, data, offset) {
+        offset = offset % data.x.length;
+        const end = Math.min((offset + 10), data.x.length);
+        return [[{x: [data.x.slice(offset, end)], y: [data.y.slice(offset, end)]}, [0], 500], end]
+    }
+    """,
+    [Output('graph', 'extendData'), Output('offset', 'data')],
+    [Input('interval', 'n_intervals')], [State('store', 'data'), State('offset', 'data')]
 )
-def update_graph(stock_slctd):
-    dff = df[df['Symbols']==stock_slctd]
-    figln = px.line(dff, x='Date', y='High')
-    return figln
 
-
-# Line chart - multiple
-@app.callback(
-    Output('line-fig2', 'figure'),
-    Input('my-dpdn2', 'value')
-)
-def update_graph(stock_slctd):
-    dff = df[df['Symbols'].isin(stock_slctd)]
-    figln2 = px.line(dff, x='Date', y='Open', color='Symbols')
-    return figln2
-
-
-# Histogram
-@app.callback(
-    Output('my-hist', 'figure'),
-    Input('my-checklist', 'value')
-)
-def update_graph(stock_slctd):
-    dff = df[df['Symbols'].isin(stock_slctd)]
-    dff = dff[dff['Date']=='2020-12-03']
-    fighist = px.histogram(dff, x='Symbols', y='Close')
-    return fighist
-
-
-if __name__=='__main__':
-    app.run_server(debug=True, port=8000)
-
-    
-# https://youtu.be/0mfIK8zxUds
+if __name__ == '__main__':
+    app.run_server()
